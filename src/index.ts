8ab3d4ec1becd3e489b4f95debf366ad317bd0ec
SifatNiloy <sifatniloy18@gmail.com>
2024-11-12 18:31:49 +0600

    cron folder and file name updated

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import app from './app';
-import { scheduleCronJobs } from './config/cron';
+import { scheduleCronJobs } from './crons/subscripStatus';
 
 dotenv.config();
 
